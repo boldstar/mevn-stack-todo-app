637a6e6452d82487e89b488cff57c62469a02cfa
Thomas <thomasweems3@gmail.com>
2019-05-29 11:38:25 -0500

    added success message handling

--- src/routes/api/todos.js
+++ src/routes/api/todos.js
@@ -9,7 +9,7 @@ const router = express.Router()
 router.get('/', (req, res) => {
     Todo.find()
     .then(todos => res.json(todos))
-    .catch(err => console.log(err))
+    .catch(err => res.status(400).json({msg: 'Oops no record found'}))
 })
 
 // @route   GET api/todos/:id
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     Todo.findOne({ _id: req.params.id})
     .then(todo => res.json(todo))
-    .catch(err => console.log(err))
+    .catch(err => res.status(400).json({msg: 'Oops no record found'}))
 })
 
 // @route   POST api/todos
@@ -34,8 +34,8 @@ router.post('/', (req, res) => {
         })
 
         newTodo.save().then(todo => {
-            res.json(todo)
-        }).catch(err => console.log(err))
+            res.json({ msg: 'Todo added', todo: todo})
+        }).catch(err => res.status(400).json({msg: 'Oops no record found'}))
     }
 
 })
@@ -54,13 +54,28 @@ router.put('/:id', (req, res) => {
 
 
 // @route   PUT api/todos/complete/:id
-// @desc    Complete record by id
+// @desc    Complete todo by id
 // @access  Public
 router.put('/complete/:id', (req, res) => {
     Todo.findOne({ _id: req.params.id}).then(todo => {
         todo.complete = !todo.complete
         todo.save()
         res.json({msg: 'Todo updated', todo})
+    }).catch(err => res.status(400).json({msg: 'Oops no record found'}))
+})
+
+// @route   POST api/todos/complete
+// @desc    Complete array of todos by id
+// @access  Public
+router.post('/complete', (req, res) => {
+    Todo.find().then(todos => {
+        for(var i = 0; i < todos.length; i++) {
+            const todo = todos.filter(todo => todo._id == req.body[i])
+            if(todo.length >= 1) {
+                todo[0].complete = true
+                todo[0].save()
+            }
+        } res.json({msg: 'Todos completed', todos: todos})
     }).catch(err => res.status(400).json({msg: 'Oops no record found'}))
 })
 
@@ -72,6 +87,20 @@ router.delete('/:id', (req, res) => {
         todo.remove()
         .then(() => res.json({msg: 'Todo deleted'}))
         .catch(err => console.log(err))
+    }).catch(err => res.status(400).json({msg: 'Oops no record found'}))
+})
+
+// @route   DELETE api/todos/delete-batch
+// @desc    Delete todos by id
+// @access  Public
+router.post('/delete-batch', (req, res) => {
+    Todo.find().then(todos => {
+        for(var i = 0; i < todos.length; i++) {
+            const todo = todos.filter(todo => todo._id == req.body[i])
+            if(todo.length >= 1) {
+                todo[0].remove()
+            }
+        } Todo.find().then(todos => res.json({ msg: 'Todos deleted', todos: todos}))
     }).catch(err => res.status(400).json({msg: 'Oops no record found'}))
 })
 
